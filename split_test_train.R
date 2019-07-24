cfccba2d33adf7b4973fefc5e9702dd8dbe7a46f
Niko Partanen <nikotapiopartanen@gmail.com>
2019-07-24 12:33:38 +0300

    updates

--- split_test_train.R
+++ split_test_train.R
@@ -1,9 +1,12 @@
-### This is Niko Partanen's example R script that splits the National
-### Library of Finland's dataset OCR Ground Truth Pages (Swedish Fraktur)
-### into line-image–text-file pairs that can be used with training
-### models with Tesseract. Same approach works easily also with Ocropy
+### This is Niko Partanen's R script that splits the National
+### Library of Finland's Fenno-Ugrica collections Alto XML files
+### into line-image–text-file pairs that can be proofread, and
+### used with training models with Tesseract. 
+### The approach is to reuse the ABBYY FineReader's line detection,
+### and just to create a new, possibly improved, OCR results to
+### accompany those
 ### Data source:
-### https://digi.kansalliskirjasto.fi/opendata
+### http://fennougrica.kansalliskirjasto.fi/handle/10024/87597
 
 library(tidyverse)
 library(xml2)
@@ -11,27 +14,22 @@ library(measurements)
 library(magick)
 library(fs)
 
-if (dir_exists("test")){
-  dir_delete("test")
-}
-
-if (dir_exists("train")){
-  dir_delete("train")
-}
-
-# We are taking a random sample when test and train set are
-# divided, so setting seed is necessary to ensure reproducibility
-
-set.seed(20170818)
 
 # This function reads Alto file and saves it as individual files.
 # Directory is simply the folder name, in this case test or train. 
 
-alto2lines <- function(xml_file, target_directory){
+# output_textfile should be .txt or .gt.txt
+
+alto2lines <- function(xml_file, target_directory, output_textfile){
   
-  if (! dir_exists(target_directory)){
-    dir_create(target_directory) 
-  }
+  # In the beginning we want an empty directory
+  # ATTENTION WITH THIS
+  
+  #if (dir_exists(target_directory)){
+  #  dir_delete(target_directory)
+  #}
+  
+  #dir_create(target_directory)
   
   xml_basename <- str_extract(xml_file, "[^/]+(?=.xml)")
   
@@ -44,48 +42,51 @@ alto2lines <- function(xml_file, target_directory){
   
   # Alto files use tenth of millimeter as the measure unit,
   # so units need to be converted to pixels -- this seems to work
+  # !! Fenno-Ugrica Alto uses pixels, this should probably be checked
+  # !! automatically.
   
-  mm10inch <- function(number){
-    measurements::conv_unit((number / 10), "mm", "inch") * 300
-  }
+  #  mm10inch <- function(number){
+  #    measurements::conv_unit((number / 10), "mm", "inch") * 300
+  #  }
   
   # This function saves individual line
   
   save_line <- function(page_image, info){
     crop_string <- str_glue("{info$width}x{info$height}+{info$hpos}+{info$vpos}")
     cropped_line <- image_crop(page_image, crop_string)
-    image_write(cropped_line, str_glue("{target_directory}/{xml_basename}-{info$order_id}.tif"))
-    write_lines(info$content, str_glue("{target_directory}/{xml_basename}-{info$order_id}.gt.txt"))
+    image_write(cropped_line, str_glue("{target_directory}/{xml_basename}-{info$order_id}.png"), format = "png")
+    write_lines(info$content, str_glue("{target_directory}/{xml_basename}-{info$order_id}{output_textfile}"))
   }
   
-  count_edits <- function(edits){
-    length(edits[edits == "true"]) / length(edits)
-  }
+#  count_edits <- function(edits){
+#    length(edits[edits == "true"]) / length(edits)
+#  }
   
-  # Saving the lines is done in the end of this function
+  # Saving the lines is done in the end of this function.
+  # But we start that only in the case there are TextLines
+  # at the page!
   
-  xml %>% xml_find_all("//TextLine") %>%
-    map_df(~ tibble(id = .x %>% xml_attr("ID"),
+  if (length(xml %>% xml_find_all("//d1:TextLine")) != 0){
+  
+  xml %>% xml_find_all("//d1:TextLine") %>%
+    map_df(~ tibble(# id = .x %>% xml_attr("ID"),
                     hpos = .x %>% xml2::xml_attr("HPOS") %>% as.numeric(),
                     vpos = .x %>% xml_attr("VPOS") %>% as.numeric(),
                     width = .x %>% xml_attr("WIDTH") %>% as.numeric(),
                     height = .x %>% xml_attr("HEIGHT") %>% as.numeric(),
-                    content = .x %>% xml_find_all("./String") %>% xml_attr("CONTENT") %>%
+                    content = .x %>% xml_find_all("./d1:String") %>% xml_attr("CONTENT") %>%
                       paste0(collapse = " "),
-                    box_id = .x %>% xml_attr("ID"),
-                    status = .x %>% xml_find_all("./String") %>% 
-                      xml_attr("CHANGED") %>%
-                      count_edits,
+                    # box_id = .x %>% xml_attr("ID"),
                     height_page = .x %>%
-                      xml_find_first("//Page") %>%
+                      xml_find_first("//d1:Page/d1:PrintSpace") %>%
                       xml_attr("HEIGHT") %>% as.numeric,
                     width_page = .x %>%
-                      xml_find_first("//Page") %>%
+                      xml_find_first("//d1:Page/d1:PrintSpace") %>%
                       xml_attr("WIDTH") %>% as.numeric)
     ) %>% # View
-    mutate_if(is.double, mm10inch) %>% # here we do mm10inch
+#    mutate_if(is.double, mm10inch) %>% # here we do mm10inch
     filter(! content == '') %>%
-    filter(! status > 0.60) %>%
+#    filter(! status > 0.60) %>%
     mutate(order_id = 1:n() %>% str_pad(width = 4, pad = "0")) %>%
     mutate(xmax = hpos + width,
            xmin = hpos,
@@ -94,12 +95,14 @@ alto2lines <- function(xml_file, target_directory){
     mutate_if(is.double, round, digits = 0) %>%
     split(.$order_id) %>%
     walk(~ {save_line(page_image = image_tif, info = .x)})
+    
+  }
   
 }
 
 # Here we list the files
 
-gt_files <- dir("postilla/testi/", 
+gt_files <- dir("kpv_3-1691_1931_DATA/", 
                 pattern = "xml", 
                 recursive = TRUE,
                 full.names = TRUE)
@@ -108,13 +111,13 @@ gt_files <- dir("postilla/testi/",
 # OCR training usually splits another test set while training,
 # but for further evaluation this is useful.
 
-test <- sample(gt_files, length(gt_files) * 0.15)
-train <- gt_files[! (gt_files %in% test)]
+# test <- sample(gt_files, length(gt_files) * 0.15)
+# train <- gt_files[! (gt_files %in% test)]
 
-# Both file lists are processed into lines
+# gt_files %>%
+#  .[5:6] %>%
+#  walk(~ {alto2lines(xml_file = .x, target_directory = "train_1", output_textfile = ".alto.txt")})
 
-test %>%
-  walk(~ {alto2lines(xml_file = .x, target_directory = "test")})
-
-train %>%
-  walk(~ {alto2lines(xml_file = .x, target_directory = "train")})
+# gt_files %>%
+#  .[7:8] %>%
+#  walk(~ {alto2lines(xml_file = .x, target_directory = "train_2", output_textfile = ".alto.txt")})
